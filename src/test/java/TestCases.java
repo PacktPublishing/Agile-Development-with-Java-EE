9b0688d23efd3e7bde6695876872dc1badcd2e06
leosamp <leo.sampaio@gmail.com>
2018-06-14 18:26:13 -0300

    Updated COURSE NOTE

--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -23,13 +23,13 @@ public class TestCases {
       Assert.assertNotNull("Apache Maven is not setup!", MAVEN_HOME);
    }
    
-   /*
    // ***** COURSE NOTE ******
    // This is the Test Case method to be added while watching 'Video 3.2: Test-driven development – Part 1'
    // Just be aware that once you add this, the build/test job will fail, as demonstrated in that same video.
    // You will learn in the next video (Video 3.3: Test-driven development – Part 2) how to 
    // make this test pass, by providing user and password in the CloudantClientMgr.java class
-   
+   // UNcomment the method by removing the /* and /* characters below:
+   /*
    @Test
    public void testDatabaseConnection(){
    	  Assert.assertNotNull("Database connection failed!",db);
